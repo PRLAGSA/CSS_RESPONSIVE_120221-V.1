3f8f211f8c2d962dca6b7d1004f2dd6ae872d6d0
Saakian <avsaakian@gmail.com>
2021-02-14 01:54:36 +0200

    FIXED BURGER V2

--- index.js
+++ index.js
@@ -24,3 +24,12 @@ function openNewWindow() {
     x.style.display = "none";
   }
 }
+
+function background() {
+  var x = document.getElementById("sideMenuTHREE");
+  if (x.style.display === "none" || x.style.display == "") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
