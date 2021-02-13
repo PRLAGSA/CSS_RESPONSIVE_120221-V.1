fae1c497b30f4a3fbc07318a6c41e2bacbf84bd4
Saakian <avsaakian@gmail.com>
2021-02-13 18:49:43 +0200

    FIXED BURGER

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function burgerOpen() {
   var x = document.getElementById("sideMenuOne");
-  if (x.style.display === "none") {
+  if (x.style.display === "none" || x.style.display == "") {
     x.style.display = "block";
   } else {
     x.style.display = "none";
