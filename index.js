fd5b5015da7b759c582dd8f2ba977168fc26acb5
Saakian <avsaakian@gmail.com>
2021-02-13 20:54:23 +0200

    x second screen ready

--- index.js
+++ index.js
@@ -15,3 +15,12 @@ function sideMenuClose() {
     x.style.display = "none";
   }
 }
+
+function openNewWindow() {
+  var x = document.getElementById("sideMenuTWO");
+  if (x.style.display === "none" || x.style.display == "") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
